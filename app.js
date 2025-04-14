46b5e4fdf9e0b277db133c2b1b6de5e9bee7706b
Daniel Jimenez Mora <djimenez@fontventa.com>
2025-04-14 16:44:27 +0200

    Commit datos

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-console.log("Hello World");
-var item = 'Bolsa';
-var num = 2;
+// BOOLEANOS
+var myBool;
+myBool = true;
+myBool = false;
+// NUMBERS
+var num = 2; //Ya sabe que es tipo Number
+var num2;
+num2 = 2;
+// STRING
+var texto;
+texto = "Hola";
+var texto1 = "Asi tambien se puede";
+//ARRAYS
+var actividades = [1, 'Hola', true];
+actividades.push('Mundo');
+actividades.push(2);
+actividades.push(false);
+var actividades2 = ["Futbol", "Baloncesto"]; //Asi le digo que es un array de string
+actividades2.push("Tenis");
--- app.ts
+++ app.ts
@@ -1,5 +1,23 @@
-console.log("Hello World");
+// BOOLEANOS
+var myBool: boolean;
+myBool = true;
+myBool = false;
 
-var item = 'Bolsa';
+// NUMBERS
+var num = 2;  //Ya sabe que es tipo Number
+var num2 : Number;
+num2 = 2;
 
-var num = 2;
+// STRING
+var texto : String;
+texto = "Hola";
+var texto1 = "Asi tambien se puede";
+
+//ARRAYS
+var actividades = [1, 'Hola', true];
+actividades.push('Mundo');
+actividades.push(2);
+actividades.push(false);
+
+var actividades2 : string[] = ["Futbol", "Baloncesto"]; //Asi le digo que es un array de string
+actividades2.push("Tenis");
